6a9ffa2e440a3c8ab22fe620cbcaf467a7bc6f19
Alvaro Felix <alvaro.felix@alumnos.udg.mx>
2020-10-20 04:40:37 -0500

    Se cambio el error del > a <

--- burbuja.go
+++ burbuja.go
@@ -1,12 +1,12 @@
 package main
 
-//import "fmt"
+import "fmt"
 
 func Burbuja(s []int64) {
 
 	for i := 0; i < len(s); i++ {
 		for j := 0; j < len(s); j++{
-			if s[i] > s[j] {
+			if s[i] < s[j] {
 				aux := s[i]
 				s[i] = s[j]
 				s[j] = aux
@@ -16,3 +16,10 @@ func Burbuja(s []int64) {
 	}
 }
 
+/*func main() {
+	s := []int64{1, -10, 90, 14, -100, -2}
+	fmt.Println(len(s))
+	Burbuja(s)
+	fmt.Println(s)
+
+}*/
