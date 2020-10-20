6cbf468f42aba000b4b2cefa51bd95ef9511a148
Alvaro Felix <alvaro.felix@alumnos.udg.mx>
2020-10-20 03:59:10 -0500

    Implementacion del metodo burbuja

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,25 @@
 package main
 
-func Burbuja(s []int64)  {
+//import "fmt"
 
+func Burbuja(s []int64) {
+
+	for i := 0; i < len(s); i++ {
+		for j := 0; j < len(s); j++{
+			if s[i] > s[j] {
+				aux := s[i]
+				s[i] = s[j]
+				s[j] = aux
+		}
+		
+		}
+	}
 }
 
-func main()  {
-	
+func main() {
+	/*sa := []int64{9, 7, 8, 10, 5, 4, 3, 2, 1, 0, -1, -2, 5}
+	fmt.Println(len(sa))
+	Burbuja(sa)
+	fmt.Println(sa)*/
+
 }
